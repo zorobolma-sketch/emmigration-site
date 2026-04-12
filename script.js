20ffa0e4798ec65ac80522295ffad38690fd9512
zorobolma-sketch <zorobolma@gmail.com>
2026-04-13 00:45:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("leadForm");
 const formMessage = document.getElementById("formMessage");
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyXCe9p4BC5tsboJUnCfmOifxMhXzJhUk-sFMJNWnOrtX2FXbTLuhZ_vi3Sc38hq-CJdA/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyBh2_sJMvaoc5poWmGMnYjpD-KQh9gp7Zc-jI9DMzjXktjuvWbrmjSxUwHr2xC2t2VDA/exec";
 const WHATSAPP_NUMBER = "447490908644";
 
 form.addEventListener("submit", async function (e) {
